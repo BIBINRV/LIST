b5f2aaecd41a888fc38f2f63fd243af5390bc442
BIBINRV <bibinrvcit@gmail.com>
2022-04-23 18:29:04 +0530

    new code

---
+++ src/org/list/Sample.java
@@ -0,0 +1,22 @@
+package org.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Sample {
+	public static void main(String[] args) {
+		List a=new ArrayList();
+		a.add(50);
+		a.add(20);
+		a.add(68);
+		a.add(78);
+		a.add(30);
+		a.add(20);
+		a.add(80);
+		a.add(95);
+		System.out.println(a);
+
+		
+	}
+
+}
